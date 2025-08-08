5b8bbf6f615ceead5b4981dfed1a8ae7fa64b3f0
MsBriteSyde <118px118@gmail.com>
2025-08-08 00:00:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 4000;
 import {employee}  from './routes/employeeRoutes.js';
 import {tasks} from './routes/taskRoutes.js';
+import cors from 'cors'
+
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/', employee);
 app.use('/api/', tasks);
@@ -13,3 +16,4 @@ app.listen(port, () => {
   console.log(`Server is running on ${port}`)
 
 })
+
