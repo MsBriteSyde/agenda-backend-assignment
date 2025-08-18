93ec7c15a49289f59a5ea3221e95324831a972ec
kelevra82 <tdmatte82@gmail.com>
2025-08-17 20:04:25 -0500

    updated index.js

--- index.js
+++ index.js
@@ -22,22 +22,18 @@ app.use("/api/", employee);
 app.use("/api/", tasks);
 
 if (environment === "production") {
-  try {
-    const options = {
-      key: fs.readFileSync(
-        "/etc/letsencrypt/live/jbethhof.codex-p4-2025.click/privkey.pem"
-      ),
-      cert: fs.readFileSync(
-        "/etc/letsencrypt/live/jbethhof.codex-p4-2025.click/fullchain.pem"
-      ),
-    };
-    https.createServer(options, app).listen(port, () => {
-      console.log(`HTTPS server is running on port ${port}`);
-    });
-  } catch (error) {
-    console.error("Failed to start HTTPS server:", error);
-    process.exit(1);
-  }
+  const options = {
+    key: fs.readFileSync(
+      "/etc/letsencrypt/live/jbethhof.codex-p4-2025.click/privkey.pem"
+    ),
+    cert: fs.readFileSync(
+      "/etc/letsencrypt/live/jbethhof.codex-p4-2025.click/fullchain.pem"
+    ),
+  };
+
+  https.createServer(options, app).listen(port, () => {
+    console.log("HTTPS server is running on port 443");
+  });
 } else {
   app.listen(port, () => {
     console.log(`listening on port ${port}`);
